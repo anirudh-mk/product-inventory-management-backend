83a2e902ae3bffcb4ddab54a41003b83cbcd439c
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-08-08 08:26:54 +0530

    product model and versatile image filed configured

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
-
+from versatileimagefield.fields import VersatileImageField
+from django.utils.translation import gettext as _
 # Create your models here.
+
+
+class Products(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ProductID = models.BigIntegerField(unique=True)
+    ProductCode = models.CharField(max_length=255, unique=True)
+    ProductName = models.CharField(max_length=255)
+    ProductImage = VersatileImageField(upload_to="uploads/", blank=True, null=True)
+    CreatedDate = models.DateTimeField(auto_now_add=True)
+    UpdatedDate = models.DateTimeField(blank=True, null=True)
+    CreatedUser = models.ForeignKey("auth.User", related_name="user%(class)s_objects", on_delete=models.CASCADE)
+    IsFavourite = models.BooleanField(default=False)
+    Active = models.BooleanField(default=True)
+    HSNCode = models.CharField(max_length=255, blank=True, null=True)
+    TotalStock = models.DecimalField(default=0.00, max_digits=20, decimal_places=8, blank=True, null=True)
+
+    class Meta:
+        db_table = "products_product"
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+        unique_together = (("ProductCode", "ProductID"),)
+        ordering = ("-CreatedDate", "ProductID")
